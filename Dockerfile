259c6cc5b48061b4192126d80c2cec10a762a6af
nerian <samslowpokd@gmail.com>
2025-03-28 08:59:08 +0000

    fix locale and +db SQLite

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,12 @@
 FROM python:3.13-slim
 
 # Устанавливаем uv и дополнительные системные пакеты
-RUN pip install uv && apt-get update && apt-get install -y vim nano curl wget git
+RUN pip install uv && apt-get update && apt-get install -y locales vim nano curl wget git sqlite3
+
+# Устнавливаем нужную кодировку
+RUN sed -i '/en_US.UTF-8/s/^# //g' /etc/locale.gen && locale-gen
+ENV LANG C.UTF-8
+ENV LC_ALL C.UTF-8
 
 # Создаём виртуальное окружение
 RUN uv venv /venv
--- src/main.py
+++ src/main.py
@@ -1,11 +1,36 @@
+import uvicorn
 from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import  Column, String
+
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+class Base(DeclarativeBase): pass
+
+class User(Base):
+    __tablename__ = "users"
+
+    email = Column(String, primary_key=True)
+    password = Column(String)
+    username = Column(String)
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello from FastAPI!"}
+#@app.get("/")
+#def read_root():
+#    return {"message": "Hello from FastAPI!"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+#@app.get("/items/{item_id}")
+#def read_item(item_id: int, q: str = None):
+#    return {"item_id": item_id, "q": q}
+
+#if __name__ == "__main__":
+ #   uvicorn.run(app, host="0.0.0.0", port=8000)
